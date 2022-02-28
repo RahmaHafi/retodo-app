df27a74eb0c23a0844425f0195f07bafe27329df
Rahma Hafi <rahma.hafi.contact@gmail.com>
2022-02-28 14:03:21 +0100

    Create to todos component and display of the initial values of the todos array

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,12 +1,31 @@
 
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
 
+function Todos(props) {
+    console.log(`todosProps`, props);
+    return (
+        <Container>
+            <ListGroup className="mt-5">
+                {
+                    props.todos.map((todo) => (
 
-import React from 'react'
+                        <ListGroup.Item key={todo.id} variant="warning" className="d-flex justify-content-between">
+                            <div>
+                                <i className="bi bi-file-check-fill btn text-warning"></i>
+                                <span>{todo.title}</span>
+                            </div>
+                            <div>
+                                <i className="bi bi-eye-fill btn text-warning"></i>
+                            </div>
+                        </ListGroup.Item>
 
-function Todos() {
-  return (
-    <div>Todos</div>
-  )
+                    ))
+                }
+            </ListGroup>
+        </Container>
+    )
 }
 
 export default Todos
