1a150d34c8961ee84b8451ee4696c511fc2fecc5
Rahma Hafi <rahma.hafi.contact@gmail.com>
2022-02-28 13:26:04 +0100

    Define the todos array initial state and passing it down to the todos component as  a props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import NavbarTodo from './components/NavbarTodo';
@@ -9,14 +9,27 @@ import TodoDetails from './pages/TodoDetails';
 
 
 function App() {
+  const [todos, setTodos] = useState([{
+    id: "1",
+    title: "Learn Node.js",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia culpa ipsum qui minima atque dolor suscipit quis cum accusamus",
+    completed: false
+  },
+  {
+    id: "2",
+    title: "Learn MongoDB",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia culpa ipsum qui minima atque dolor suscipit quis cum accusamus",
+    completed: false
+  }])
+
+
   return (
     <Router>
-
-        <NavbarTodo />
-        <Switch>
-        <Route  exact path="/" component={Todos}/>
-        <Route  path="/new-todo" component={Todo}/>
-        <Route  path="/todos/:id" component={TodoDetails}/>
+      <NavbarTodo />
+      <Switch>
+        <Route exact path="/" component={(props)=> <Todos {...props} todos={todos}/> } />
+        <Route path="/new-todo" component={Todo} />
+        <Route path="/todos/:id" component={TodoDetails} />
       </Switch>
     </Router>
   );
